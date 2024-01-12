bfed7b26c2f9975de615a53f317e2f6bf92b35c6
SamInst <sam105666@ceuma.com.br>
2024-01-12 16:45:08 -0300

    refatorado metodos de pesquisa

--- src/GenerateLotoEasyNumbers.java
+++ src/GenerateLotoEasyNumbers.java
@@ -2,202 +2,54 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
 public class GenerateLotoEasyNumbers {
     public static void main(String[] args) {
+        Integer[] myNumbers = {2, 3, 5, 6, 7, 8, 10, 14, 15, 16, 18, 19, 21, 23, 24};
+        List<Integer> myNumbersList = Arrays.asList(myNumbers);
 
-            int count = 0;
-            int count2 = 0;
-            Integer[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
-            List<Integer> list = Arrays.asList(values);
-            Collections.shuffle(list);
+        Integer[] myNumbers2 = {1, 2, 3, 4, 6, 7, 8, 11, 12, 13, 17, 19, 22, 23, 24};
+        List<Integer> myNumbersList2 = Arrays.asList(myNumbers2);
 
-            Integer[] myNumbers = {2, 3, 5, 6, 7, 8, 10, 14, 15, 16, 18, 19, 21, 23, 24};
-            List<Integer> mynumberslist = Arrays.asList(myNumbers);
-            System.out.println("Meus números: " + mynumberslist);
+        int count;
+        int count2;
+        int totalGames = 0;
 
-            Integer[] myNumbers2 = {1, 2, 3, 4, 6, 7, 8, 11, 12, 13, 17, 19, 22, 23, 24};
-            List<Integer> mynumberslist2 = Arrays.asList(myNumbers2);
-            System.out.println("Meus números 2: " + mynumberslist2);
+        do {
+            totalGames++;
 
-            List<Integer> values2 = new ArrayList<>();
-            values2.add(list.get(0));
-            values2.add(list.get(1));
-            values2.add(list.get(2));
-            values2.add(list.get(3));
-            values2.add(list.get(4));
-            values2.add(list.get(5));
-            values2.add(list.get(6));
-            values2.add(list.get(7));
-            values2.add(list.get(8));
-            values2.add(list.get(9));
-            values2.add(list.get(10));
-            values2.add(list.get(11));
-            values2.add(list.get(12));
-            values2.add(list.get(13));
-            values2.add(list.get(14));
+            List<Integer> list = generateRandomNumbers();
 
-            if (mynumberslist.contains(1) && values2.contains(1)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(2) && values2.contains(2)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(3) && values2.contains(3)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(4) && values2.contains(4)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(5) && values2.contains(5)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(6) && values2.contains(6)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(7) && values2.contains(7)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(8) && values2.contains(8)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(9) && values2.contains(9)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(10) && values2.contains(10)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(11) && values2.contains(11)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(12) && values2.contains(12)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(13) && values2.contains(13)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(14) && values2.contains(14)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(15) && values2.contains(15)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(16) && values2.contains(16)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(17) && values2.contains(17)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(18) && values2.contains(18)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(19) && values2.contains(19)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(20) && values2.contains(20)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(21) && values2.contains(21)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(22) && values2.contains(22)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(23) && values2.contains(23)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(24) && values2.contains(24)) {
-                count += 1;
-            }
-            if (mynumberslist.contains(25) && values2.contains(25)) {
-                count += 1;
-            }
-
-            if (mynumberslist2.contains(1) && values2.contains(1)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(2) && values2.contains(2)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(3) && values2.contains(3)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(4) && values2.contains(4)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(5) && values2.contains(5)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(6) && values2.contains(6)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(7) && values2.contains(7)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(8) && values2.contains(8)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(9) && values2.contains(9)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(10) && values2.contains(10)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(11) && values2.contains(11)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(12) && values2.contains(12)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(13) && values2.contains(13)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(14) && values2.contains(14)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(15) && values2.contains(15)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(16) && values2.contains(16)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(17) && values2.contains(17)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(18) && values2.contains(18)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(19) && values2.contains(19)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(20) && values2.contains(20)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(21) && values2.contains(21)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(22) && values2.contains(22)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(23) && values2.contains(23)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(24) && values2.contains(24)) {
-                count2 += 1;
-            }
-            if (mynumberslist2.contains(25) && values2.contains(25)) {
-                count2 += 1;
-            }
-
+            List<Integer> values2 = list.subList(0, 15);
             Collections.sort(values2);
 
+            count = countMatches(myNumbersList, values2);
+            count2 = countMatches(myNumbersList2, values2);
 
+            if (count > 12 || count2 > 12) {
+                System.out.println("Sorteados: " + values2);
+                System.out.println("Acertados: " + count);
+                System.out.println("Acertados na lista 2: " + count2);
+                System.out.println("Total de jogos até o acerto: " + totalGames);
+            }
 
-                System.out.println("sorteados:    "+values2);
-                System.out.println("acertados: "+ count);
-                System.out.println("acertados na lista 2: "+count2);
+        } while (count < 15 && count2 < 15);
+    }
 
+    private static List<Integer> generateRandomNumbers() {
+        Integer[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+        List<Integer> list = new ArrayList<>(Arrays.asList(values));
+        Collections.shuffle(list);
+        return list;
+    }
 
-
-
+    private static int countMatches(List<Integer> list1, List<Integer> list2) {
+        int count = 0;
+        for (Integer number : list1) {
+            if (list2.contains(number)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
--- src/GenerateMegaSenaNumbers.java
+++ src/GenerateMegaSenaNumbers.java
@@ -5,30 +5,46 @@ import java.util.List;
 
 public class GenerateMegaSenaNumbers {
     public static void main(String[] args) {
+        Integer[] myNumbers = {1, 3, 5, 14, 56, 60};
+        List<Integer> myNumbersList = Arrays.asList(myNumbers);
 
-        int[] newArray = new int[10];
+        int count;
+        int totalGames = 0;
 
-        for(int i = 0; i < newArray.length; i++) {
-;
+        do {
+            totalGames++;
 
+            List<Integer> list = generateRandomNumbers(60);
+            Collections.sort(list);
 
-            System.out.println(newArray);
+            count = countMatches(myNumbersList, list);
 
+            if (count >= 4) {
+                System.out.println("Sorteados: " + list);
+                System.out.println("Acertados: " + count);
+                System.out.println("Total de jogos até o acerto: " + totalGames);
+                System.out.println();
+            }
+
+        } while (count < 6);
+    }
+
+    private static List<Integer> generateRandomNumbers(int range) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= range; i++) {
+            list.add(i);
         }
-        //        Integer [] values = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
-//                    31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60};
-//        List<Integer> list = Arrays.asList(values);
-//        Collections.shuffle(list);
-//
-//        List<Integer> values2 = new ArrayList<>();
-//        values2.add(list.get(0));
-//        values2.add(list.get(1));
-//        values2.add(list.get(2));
-//        values2.add(list.get(3));
-//        values2.add(list.get(4));
-//        values2.add(list.get(5));
-//
-//        Collections.sort(values2);
-//        System.out.println(values2);
+        Collections.shuffle(list);
+        return list.subList(0, 6);
+    }
+
+    private static int countMatches(List<Integer> list1, List<Integer> list2) {
+        int count = 0;
+        for (Integer number : list1) {
+            if (list2.contains(number)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
